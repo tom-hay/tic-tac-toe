5dbacbf52c72270d0f30631c12f523d39dc8c243
Tom Haywood <tom.a.haywood@gmail.com>
2020-07-13 16:47:56 +1000

    ran linter

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'lib/tic_tac_toe'
 
 game = TicTacToe.new
--- test/tic_tac_toe_test.rb
+++ test/tic_tac_toe_test.rb
@@ -11,31 +11,31 @@ class TicTacToeTest < Minitest::Test
 
   def test_that_board_with_2_x_and_2_o_is_valid
     game = TicTacToe.new(['X', 'X', ' ', 'O', ' ', 'O', ' ', ' ', ' '])
-    assert_equal "X", game.current_player
+    assert_equal 'X', game.current_player
   end
 
   def test_that__board_with_one_x_and_3_os_is_invalid
     game = TicTacToe.new(['X', 'X', ' ', 'O', 'X', ' ', ' ', ' ', 'O'])
-    assert_equal "O", game.current_player
+    assert_equal 'O', game.current_player
   end
 
   def test_that_3_in_a_row_horizontal_is_a_win
     game = TicTacToe.new(['X', 'X', 'X', ' ', 'O', 'O', ' ', ' ', 'O'])
-    assert_equal "X", game.won?
+    assert_equal 'X', game.won?
   end
 
   def test_that_3_in_a_row_vertical_is_a_win
     game = TicTacToe.new(['O', 'X', ' ', ' ', 'X', ' ', 'O', 'X', 'O'])
-    assert_equal "X", game.won?
+    assert_equal 'X', game.won?
   end
 
   def test_that_3_in_a_row_diagonal_is_a_win
     game = TicTacToe.new(['X', 'O', 'O', ' ', 'X', ' ', ' ', 'O', 'X'])
-    assert_equal "X", game.won?
+    assert_equal 'X', game.won?
   end
 
   def test_that_full_board_and_no_win_is_a_draw
-    game = TicTacToe.new(['X', 'X', 'O', 'O', 'X', 'X', 'X', 'O', 'O'])
+    game = TicTacToe.new(%w[X X O O X X X O O])
     assert_equal true, game.draw?
   end
 end
